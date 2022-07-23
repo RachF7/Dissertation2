4d770fcb70dfca83a9df9d6dca77a23035f0f6f6
Rachael Ferguson <2252146F@student.gla.ac.uk>
2022-07-23 09:04:28 +0100

    map_working

--- diss/admin.py
+++ diss/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Diss
+
+
+@admin.register(Diss)
+class ShopAdmin(OSMGeoAdmin):
+    list_display = ('name', 'location')
+
+
+
 
 # Register your models here.
--- diss_project/settings.py
+++ diss_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'diss',
 ]
 
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'diss_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'swimlocations',
+        'USER': 'admin',
+        'PASSWORD': 'swimpass',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
